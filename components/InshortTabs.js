7f0914e891aadc3276c7be2c1c0662f2bf37fa2c
lenovodeez1 <deebisht98>
2022-04-18 01:24:05 +0530

    added tab view dependency

---
+++ components/InshortTabs.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function InshortTabs() {
+  return (
+    <View>
+      <Text>InshortTabs</Text>
+    </View>
+  );
+}
